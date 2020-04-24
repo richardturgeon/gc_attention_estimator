a1960a06fec097c186d8b62548c9d9f1bdb7991a
richardturgeon <richard.turgeon@comcast.net>
2020-04-24 13:51:13 -0700

    forgot this guy

---
+++ tfrecord_data.py
@@ -0,0 +1,99 @@
+import os
+import tensorflow as tf
+
+# ------------------------------------------------------------------------------
+#   Estimator Input function (input_fn)
+# ------------------------------------------------------------------------------
+
+def input_fn(params):
+    """Return dataset iterator.
+
+    Pending:
+        implement class weights
+        test predict, i.e. partition=test
+
+    Args:
+        data_dir
+        batch_size
+        partition           One of train, val, test
+        params
+    """
+
+    data_dir = params['data_dir']
+    batch_size = params['batch_size']
+    partition = params['partition']
+
+    if partition == 'train':
+        is_training = True
+    else:
+        is_training = False
+
+    epochs = params['epochs']
+    file_prefix = params['file_prefix']
+    shuffle_buffer = params['shuffle_buffer']
+    cell_input_size, drug_input_size = params['input_sizes']
+
+    file_pattern = os.path.join(data_dir, f'{file_prefix}{partition}*.tfrecords')
+    dataset = tf.data.Dataset.list_files(file_pattern, shuffle=True)
+    dataset = dataset.flat_map(tf.data.TFRecordDataset)
+
+    def _parse_record_fn(raw_record):
+        """Decode raw TFRecord into feature and label components."""
+        """
+        feature_map = {
+            'data':  tf.io.FixedLenFeature([data_sz], dtype=tf.float32),
+            'label': tf.io.FixedLenFeature([1], dtype=tf.int64)
+        }
+
+        record_features = tf.io.parse_single_example(raw_record, feature_map)
+
+        label = record_features['label']
+        class_weights = params['class_weights']
+        tensor_weights = tf.constant(class_weights)
+        #record_features['weights'] = tf.gather(tensor_weights, label)
+        label_int32 = tf.cast(record_features['label'], dtype=tf.int32)
+        record_features.pop('label')
+        return record_features, label_int32
+        """
+
+        feature_map = {
+            'cell': tf.io.FixedLenFeature([cell_input_size], dtype=tf.float32),
+            'drug': tf.io.FixedLenFeature([drug_input_size], dtype=tf.float32),
+            'AUC':  tf.io.FixedLenFeature([1], dtype=tf.float32)
+        }
+
+        record_features = tf.io.parse_single_example(raw_record, feature_map)
+
+        features = {}
+        cell = record_features['cell']
+        drug = record_features['drug']
+        features = tf.concat([cell, drug], axis=0)
+        features = tf.cast(features, dtype=tf.float32)
+        label = record_features['AUC']
+        label = tf.cast(label, dtype=tf.int32)
+        return features, label
+
+    return process_record_dataset(dataset, is_training, batch_size, shuffle_buffer, _parse_record_fn, num_epochs=epochs)
+
+
+def process_record_dataset(dataset, is_training, batch_size, shuffle_buffer, parse_record_fn, num_epochs=None):
+    """Given a Dataset with raw records, return an iterator over the records."""
+
+    dataset = dataset.prefetch(buffer_size=batch_size)
+
+    if is_training:
+        dataset = dataset.shuffle(buffer_size=shuffle_buffer)
+        dataset = dataset.repeat(count=None)    # forever, a CS-1 rqmt
+
+    dataset = dataset.apply(
+#       tf.contrib.data.map_and_batch(
+        tf.data.experimental.map_and_batch(
+            lambda raw_record: parse_record_fn(raw_record),
+            batch_size=batch_size,
+#           num_parallel_batches=1,
+            num_parallel_calls=tf.contrib.data.AUTOTUNE,        # lots of warnings re inability to improve ... 
+            drop_remainder=True))
+
+    dataset = dataset.prefetch(buffer_size=tf.contrib.data.AUTOTUNE)
+    return dataset
+
